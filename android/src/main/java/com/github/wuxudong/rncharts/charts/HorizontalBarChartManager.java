a0ef9bad9919510fd37cec83648536ed8b3fe400
huybq95 <myheaven291@gmail.com>
2018-02-08 14:37:45 +0700

    Update HorizontalBarChartManager.java

--- android/src/main/java/com/github/wuxudong/rncharts/charts/HorizontalBarChartManager.java
+++ android/src/main/java/com/github/wuxudong/rncharts/charts/HorizontalBarChartManager.java
@@ -2,10 +2,12 @@ package com.github.wuxudong.rncharts.charts;
 
 import android.view.View;
 
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.wuxudong.rncharts.data.BarDataExtract;
 import com.github.wuxudong.rncharts.data.DataExtract;
@@ -25,5 +27,11 @@ public class HorizontalBarChartManager extends BarChartManager {
         horizontalBarChart.setOnChartValueSelectedListener(new RNOnChartValueSelectedListener(horizontalBarChart));
         horizontalBarChart.setOnChartGestureListener(new RNOnChartGestureListener(horizontalBarChart));
         return horizontalBarChart;
+    }
+
+    @ReactProp(name = "extraOffsets")
+    public void setExtraOffsets(PieChart chart, ReadableArray array) {
+        chart.setExtraOffsets((float) array.getDouble(0), (float) array.getDouble(1),
+                (float) array.getDouble(2), (float) array.getDouble(3));
     }
 }
