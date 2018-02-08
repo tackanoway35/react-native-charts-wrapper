3cb19be4c1a9b58a1b95b628f758482e61399f0c
huybq95 <myheaven291@gmail.com>
2018-02-08 10:58:20 +0700

    Update RNPieChartManagerBridge.m

--- ios/ReactNativeCharts/pie/RNPieChartManagerBridge.m
+++ ios/ReactNativeCharts/pie/RNPieChartManagerBridge.m
@@ -8,10 +8,11 @@
 @interface RCT_EXTERN_MODULE(RNPieChartManager, RCTViewManager)
 
 EXPORT_CHART_BASE_PROPERTIES
+RCT_EXPORT_VIEW_PROPERTY(extraOffsets, NSNumberArray)
 RCT_EXPORT_VIEW_PROPERTY(drawEntryLabels, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(usePercentValues, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(xValuePosition, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(centerText, NSString)
-RCT_EXPORT_VIEW_PROPERTY(styledCenterText, NSDictionary)
 RCT_EXPORT_VIEW_PROPERTY(centerTextRadiusPercent, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(holeRadius, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(holeColor, NSInteger)
@@ -19,10 +20,12 @@ RCT_EXPORT_VIEW_PROPERTY(transparentCircleRadius, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(transparentCircleColor, NSInteger)
 RCT_EXPORT_VIEW_PROPERTY(entryLabelColor, NSInteger)
 RCT_EXPORT_VIEW_PROPERTY(entryLabelTextSize, NSNumber)
+RCT_EXPORT_VIEW_PROPERTY(valueLineWidth, NSNumber)
+RCT_EXPORT_VIEW_PROPERTY(valueLinePart1Length, NSNumber)
+RCT_EXPORT_VIEW_PROPERTY(valueLinePart2Length, NSNumber)
+RCT_EXPORT_VIEW_PROPERTY(valueLineColor, NSNumber)
 
 RCT_EXPORT_VIEW_PROPERTY(maxAngle, NSNumber)
-RCT_EXPORT_VIEW_PROPERTY(minOffset, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(rotationEnabled, BOOL)
-RCT_EXPORT_VIEW_PROPERTY(rotationAngle, NSNumber)
 
 @end
